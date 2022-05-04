7859017089518f81d3d5a6cb2e52d295c367b3e9
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-05-04 08:35:17 +0300

    PEP8 corrections made

--- functions.py
+++ functions.py
@@ -2,40 +2,54 @@ import random
 
 from datacenter.models import Schoolkid, Chastisement, Lesson, Commendation, Mark
 
+
 def get_child(name):
     try:
-        return Schoolkid.objects.get(full_name__contains = name)
+        return Schoolkid.objects.get(full_name__contains=name)
     except Schoolkid.DoesNotExist:
         print('Такого ученика нет в базе данных.\n Проверьте имя')
     except Schoolkid.MultipleObjectsReturned:
         print('Найдено несколько учеников. Уточните имя')
 
-def fix_marks(schoolkid):
-    Mark.objects.filter(schoolkid = schoolkid).filter(points__lt = 4).update(points = 5)
 
-def  remove_chastisements(schoolkid):
-    Chastisement.objects.filter(schoolkid = schoolkid).delete()
-    
+def fix_marks(schoolkid):
+    Mark.objects.filter(
+        schoolkid=schoolkid).filter(
+            points__lt=4
+        ).update(points=5)
+
+
+def remove_chastisements(schoolkid):
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
+
+
 def create_commendation(child, subject):
     text = random.choice([
         'Молодец!',
         "Отлично!",
         "Хорошо!",
         "Гораздо лучше, чем я ожидал!",
-        "Сказано здорово – просто и ясно!", 
+        "Сказано здорово – просто и ясно!",
         "Уже существенно лучше!"
     ])
     try:
-        lesson1 = Lesson.objects.filter(group_letter = child.group_letter
-                                        ).filter(year_of_study = child.year_of_study
-                                                ).filter(subject__title__icontains = subject
-                                                        ).order_by('-date')[0]
+        lesson = (
+            Lesson.objects.filter(
+                group_letter=child.group_letter,
+                year_of_study=child.year_of_study,
+                subject__title=subject
+            )
+            .order_by('-date')[0]
+        )
     except IndexError:
-        print('Не удалось найти ни одного такого урока у этого ученика.\n Проверьте название предмета')
+        print(
+            'Не удалось найти ни одного такого урока у этого ученика.\n Проверьте название предмета'
+        )
         return
     Commendation.objects.create(
-        text = text, 
-        subject = lesson1.subject, 
-        teacher = lesson1.teacher, 
-        created = lesson1.date, 
-        schoolkid = child)
+        text=text,
+        subject=lesson.subject,
+        teacher=lesson.teacher,
+        created=lesson.date,
+        schoolkid=child
+    )
